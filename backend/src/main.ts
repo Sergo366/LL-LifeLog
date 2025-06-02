0f96ca9471cc811dadd169349dd7f0339e2d881e
skozenkov <kozenkovs366@gmail.com>
2025-06-02 12:27:25 +0200

    setting for redis and new utils

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,34 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import IORedis from 'ioredis';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	const config = app.get(ConfigService);
-	app.use(cookieParser(config.getOrThrow<string>('COOKIE_SECRET')));
 
+	const config = app.get(ConfigService);
+	const redis = new IORedis(config.getOrThrow<string>('REDIS_URI'));
+
+	app.use(cookieParser(config.getOrThrow<string>('COOKIE_SECRET')));
 	app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: true,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				maxAge: config.getOrThrow<number>('SESSION_MAX_AGE'),
+				httpOnly: true,
+				sameSite: 'lax',
+				secure: config.getOrThrow<boolean>('IS_PRODUCTION')
+			}
+		})
+	);
 	app.enableCors({
 		origin: config.getOrThrow<string>('ALLOWED_ORIGINS'),
 		credentials: true,
