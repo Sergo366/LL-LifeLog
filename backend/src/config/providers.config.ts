1d83482b98a878ab34cf4aa96352eb610895c5f7
skozenkov <kozenkovs366@gmail.com>
2025-06-07 11:12:29 +0200

    deleted yandex.provider.ts

--- backend/src/config/providers.config.ts
+++ backend/src/config/providers.config.ts
@@ -1,7 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOptions } from '@/auth/provider/provider.constants';
 import { GoogleProvider } from '@/auth/provider/services/google.provider';
-import { YandexProvider } from '@/auth/provider/services/yandex.provider';
 
 export const getProvidersConfig = async (
 	configService: ConfigService
@@ -12,11 +11,6 @@ export const getProvidersConfig = async (
 			client_id: configService.getOrThrow<string>('GOOGLE_CLIENT_ID'),
 			client_secret: configService.getOrThrow<string>('GOOGLE_CLIENT_SECRET'),
 			scopes: ['email', 'profile']
-		}),
-		new YandexProvider({
-			client_id: configService.getOrThrow<string>('YANDEX_CLIENT_ID'),
-			client_secret: configService.getOrThrow<string>('YANDEX_CLIENT_SECRET'),
-			scopes: ['login:email', 'login:avatar', 'login:info']
 		})
 	]
 });
