022233b06ebb9801726b746f49e81761487fc8ac
skozenkov <kozenkovs366@gmail.com>
2025-06-10 18:52:51 +0200

    update and remove user

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -23,11 +23,20 @@ export class UsersService {
     return this.repo.find({ where: { email } });
   }
 
-  update(id: number, email: string, password: string) {
-    console.log('id', id, 'email', email, 'password', password);
+  async update(id: number, attrs: Partial<UserEntity>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
   }
 
-  remove(id: number) {
-    console.log('id', id);
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return this.repo.remove(user);
   }
 }
