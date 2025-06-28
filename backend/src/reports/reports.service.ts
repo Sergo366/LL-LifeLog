3a05ee723fdea9aeab23786fff3239d17eb95e58
skozenkov <kozenkovs366@gmail.com>
2025-06-28 12:43:04 +0200

    dto, controller and DI

--- backend/src/reports/reports.service.ts
+++ backend/src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ReportsEntity } from './reports.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(ReportsEntity) private repo: Repository<ReportsEntity>,
+  ) {}
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
+  }
+}
