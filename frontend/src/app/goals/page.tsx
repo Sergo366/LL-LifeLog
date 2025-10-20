20e1189ae3dbfbaaa872bad4924529b0361150a8
serhii <kozenkovs366@gmail.com>
2025-10-20 09:29:59 +0200

    front and back

--- frontend/src/app/goals/page.tsx
+++ frontend/src/app/goals/page.tsx
@@ -1,7 +1,29 @@
+'use client'
+import { Button } from '@mui/material';
 import React from 'react';
+import classes from './styles.module.css'
 
 const GoalsPage = () => {
-  return <div>GoalsPage</div>;
+	const data = [
+		{id: 0, name: 'Buy new trainers', dueDate: '2025/12/31', createdAt: '2025/10/19', progressState: 'progress' },
+	]
+  return <div className={classes.wrapper}>
+		<div className={classes.goalHeader}>
+			<Button variant="outlined">Add new goal</Button>
+		</div>
+		<div className={classes.goalContent}>
+			{data.map((goal, index) => (
+				<div key={goal.id} className={classes.element}>
+					{index}
+					{goal.name}
+					{goal.dueDate}
+					{goal.createdAt}
+					<Button variant={'outlined'} color={'info'}>Update</Button>
+					<Button variant={'outlined'} color="error">Delete</Button>
+				</div>
+			))}
+		</div>
+	</div>;
 };
 
 export default GoalsPage;
--- frontend/src/entities/Header/Header.tsx
+++ frontend/src/entities/Header/Header.tsx
@@ -1,16 +1,17 @@
 import classes from './styles.module.css';
-import { Logo } from './Logo';
+import Logo from '../../../public/LifeLogLogo.svg'
 import Link from 'next/link';
 import { navigationList } from '@/entities/Header/const';
 import { routes } from '@/shared/routes';
 import { HeaderItem } from '@/entities/Header/HeaderItem';
+import Image from 'next/image';
 
 export const Header = () => {
   return (
     <header className={classes.wrapper}>
       <nav className={classes.navigation}>
         <Link href={routes.home}>
-          <Logo />
+          <Image src={Logo} alt="logo" height={150} width={150} />
         </Link>
         <ul className={classes.navList}>
           {navigationList.map((nav) => (
