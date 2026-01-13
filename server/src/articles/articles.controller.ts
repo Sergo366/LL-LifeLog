92f28db4def46994b7b59d6f8e6adc8c3b154d12
serhii <kozenkovs366@gmail.com>
2026-01-13 10:55:51 +0200

    ROLES

--- server/src/articles/articles.controller.ts
+++ server/src/articles/articles.controller.ts
@@ -6,16 +6,23 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { AuthGuard } from '../auth/guard/auth.guard';
+import { EUserRole } from '../shared/models/user.entity';
+import { Roles } from '../shared/decorators/roles.decorator';
+import { RolesGuard } from '../shared/guards/roles.guard';
 
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly service: ArticlesService) {}
 
   @Post()
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(EUserRole.ADMIN)
   create(@Body() data: CreateArticleDto) {
     return this.service.create(data);
   }
@@ -31,11 +38,15 @@ export class ArticlesController {
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(EUserRole.ADMIN)
   updateById(@Param('id') id: number, @Body() data: UpdateArticleDto) {
     return this.service.updateById(id, data);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(EUserRole.ADMIN)
   deleteById(@Param('id') id: number) {
     return this.service.deleteById(id);
   }
