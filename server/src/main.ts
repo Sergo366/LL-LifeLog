10c74122b3acc0e45f22de8b0aca2d354786cd27
serhii <kozenkovs366@gmail.com>
2026-01-28 10:23:32 +0200

    commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-const PORT = 3000;
+const PORT = 5001;
 const HOST = '127.0.0.1';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   await app.listen(PORT, HOST, () => {
     console.log(`Server started on port http://${HOST}:${PORT}`);
   });
